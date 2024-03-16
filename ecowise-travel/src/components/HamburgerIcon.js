c1ee2c78832a258aa861e29c7b908219d995248e
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-17 00:16:02 +0100

    Update HamburgerIcon.js

--- ecowise-travel/src/components/HamburgerIcon.js
+++ ecowise-travel/src/components/HamburgerIcon.js
@@ -1,13 +1,31 @@
 // HamburgerIcon.js
+
 import React from 'react';
 
-const HamburgerIcon = ({ onClick }) => (
-  <div className="hamburger-icon" onClick={onClick}>
-    <div className="line"></div>
-    <div className="line"></div>
-    <div className="line"></div>
-  </div>
-);
+const HamburgerIcon = ({ isOpen, onClick }) => {
+  const barStyle = {
+    width: '25px',
+    height: '3px',
+    backgroundColor: '#333',
+    margin: '5px 0',
+    transition: '0.4s',
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    height: '24px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={containerStyle} onClick={onClick}>
+      <div style={{ ...barStyle, transform: isOpen ? 'rotate(-45deg) translate(-5px, 6px)' : 'none' }}></div>
+      <div style={{ ...barStyle, opacity: isOpen ? '0' : '1', transitionDelay: isOpen ? '0' : '0.2s' }}></div>
+      <div style={{ ...barStyle, transform: isOpen ? 'rotate(45deg) translate(-5px, -6px)' : 'none' }}></div>
+    </div>
+  );
+};
 
 export default HamburgerIcon;
-
