c35b26dbdfd0c1d638deb9f8b20c4525db2e7b8d
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-16 22:52:14 +0100

    Updated

--- ecowise-travel/src/App.js
+++ ecowise-travel/src/App.js
@@ -18,14 +18,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/search" element={<AdventureSearchPage />} />
-          <Route path="/impact-calculator" element={<ImpactCalculator />} />
-          <Route path="/tips" element={<TravelTips />} />
-          <Route path="/forum" element={<CommunityForum />} />
           <Route path="/reviews" element={<ReviewPage />} />
           {/* Add routes for new paths */}
           <Route path="/adventures" element={<AdventureSearchPage />} />
           <Route path="/community-forum" element={<CommunityForum />} />
           <Route path="/impact-calculator" element={<ImpactCalculator />} />
+          <Route path="*" element={null} />
         </Routes>
         <Footer />
       </div>
