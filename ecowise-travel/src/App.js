60dbe3e2eb0efbbe899f69921030432f130fc609
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-17 00:41:35 +0100

    Update App.js

--- ecowise-travel/src/App.js
+++ ecowise-travel/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HamburgerIcon from './components/HamburgerIcon';
 import HomePage from './components/HomePage';
 import AdventureSearchPage from './components/AdventureSearchPage';
 import ImpactCalculator from './components/ImpactCalculator';
@@ -12,17 +11,10 @@ import ReviewPage from './components/ReviewPage';
 import './App.css';
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu open/close
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Function to toggle menu state
-  };
-
   return (
     <Router>
       <div className="App">
         <Header />
-        <HamburgerIcon isOpen={isMenuOpen} onClick={toggleMenu} />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/search" element={<AdventureSearchPage />} />
