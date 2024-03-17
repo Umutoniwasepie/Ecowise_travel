e64833dcd67c85b729a359fb4071697a1968e3f7
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-17 01:24:36 +0100

    Update Header.js

--- ecowise-travel/src/components/Header.js
+++ ecowise-travel/src/components/Header.js
@@ -2,19 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-     <div className="homepage-container">
-            {/* Header */}
-            <header className="header">
-                <div className="logo">
-                    <img src="ecowise-logo.png" alt="EcoWise Travel Logo" />
-                </div>
-     </div>
-      {/* Header content goes here */}
-      <h1>EcoWise Travel</h1>
-      <nav>
-        {/* Navigation links can go here */}
-      </nav>
+    <header className="header">
+      <div className="homepage-container">
+        <div className="logo">
+          <img src="ecowise-logo.png" alt="EcoWise Travel Logo" />
+        </div>
+        {/* Header content goes here */}
+        <h1>EcoWise Travel</h1>
+        <nav>
+          {/* Navigation links can go here */}
+        </nav>
+      </div>
     </header>
   );
 };
