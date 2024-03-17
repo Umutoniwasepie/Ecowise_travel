6ee3e073c009db92b9aba05566aa3d86d9b1b150
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-17 13:48:58 +0100

    Update commentRoutes.js

--- ecowise-travel/src/backend/src/routes/commentRoutes.js
+++ ecowise-travel/src/backend/src/routes/commentRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const CommentController = require('../controllers/CommentController');
 
 // Routes for Comment resource
-router.get('/comments', CommentController.getAllComments);
-router.get('/comments/:id', CommentController.getCommentById);
-router.post('/comments', CommentController.createComment);
-router.put('/comments/:id', CommentController.updateComment);
-router.delete('/comments/:id', CommentController.deleteComment);
+router.get('/', CommentController.getAllComments);
+router.get('/:id', CommentController.getCommentById);
+router.post('/', CommentController.createComment);
+router.put('/:id', CommentController.updateComment);
+router.delete('/:id', CommentController.deleteComment);
 
 module.exports = router;
 
