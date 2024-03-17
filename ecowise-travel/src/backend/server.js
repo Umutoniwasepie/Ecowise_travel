aa8b845c68d658d3a21419d176f55aba010054ce
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-17 18:51:57 +0100

    Update server.js

--- ecowise-travel/src/backend/server.js
+++ ecowise-travel/src/backend/server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 require('dotenv').config();
 const connectDB = require('./db'); // Corrected path
-const userRoutes = require('./routes/userRoutes'); // Import user routes
-const postRoutes = require('./routes/postRoutes'); // Import post routes
-const commentRoutes = require('./routes/commentRoutes'); // Import comment routes
+const userRoutes = require('./src/routes/userRoutes'); // Import user routes
+const postRoutes = require('./src/routes/postRoutes'); // Import post routes
+const commentRoutes = require('./src/routes/commentRoutes'); // Import comment routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
