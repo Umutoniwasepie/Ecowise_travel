71999708dc42386933c109ccbe14cd77b016f978
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-23 14:57:05 +0100

    Update server.js

--- ecowise-travel/backend/server.js
+++ ecowise-travel/backend/server.js
@@ -1,8 +1,9 @@
+// server.js
+
 const express = require('express');
 const dotenv = require('dotenv');
-const { Pool } = require('pg');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
-// Import other routes as needed
+const sequelize = require('./config/database'); // Import Sequelize instance
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,15 +15,6 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 
-// PostgreSQL database connection configuration
-const pool = new Pool({
-  user: process.env.ecowise_admin,
-  host: process.env.localhost,
-  database: process.env.ecowise_travel,
-  password: process.env.Ec0w!seP@ss,
-  port: process.env.3000,
-});
-
 // Mount routes
 app.use('/api/users', userRoutes);
 // Mount other routes as needed
@@ -32,7 +24,14 @@ app.get('/', (req, res) => {
   res.send('Hello from your Express server!');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Sync Sequelize models with the database
+sequelize.sync()
+  .then(() => {
+    // Start the server after syncing models
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing Sequelize models:', error);
+  });
