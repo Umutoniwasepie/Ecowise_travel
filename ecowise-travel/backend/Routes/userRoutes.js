c01cf5f9a5b685296b240153abf88fa02855eda5
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-23 15:20:06 +0100

    Update userRoutes.js

--- ecowise-travel/backend/Routes/userRoutes.js
+++ ecowise-travel/backend/Routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require('../Controllers/UserController');
 
 // Routes for User resource
 router.get('/', UserController.getAllUsers);
