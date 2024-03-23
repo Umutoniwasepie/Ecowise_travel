a3cb7fb62176cd989964273145f323de7c391296
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-23 14:47:39 +0100

    Update User.js

--- ecowise-travel/backend/models/User.js
+++ ecowise-travel/backend/models/User.js
@@ -4,8 +4,25 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
-  // Define user model attributes
+  // Define the fields of the users table
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = User;
-
