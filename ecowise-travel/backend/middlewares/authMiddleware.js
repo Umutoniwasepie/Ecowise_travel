ea40f226ff3387a97adb1adb6f349048fd98302f
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-23 19:22:20 +0100

    Update authMiddleware.js

--- ecowise-travel/backend/middlewares/authMiddleware.js
+++ ecowise-travel/backend/middlewares/authMiddleware.js
@@ -1,1 +1,22 @@
-i
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  const secretKey = process.env.JWT_SECRET; // Access the secret key from the .env file
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = decoded; // Attach decoded user data to the request object
+    next();
+  });
+};
+
+module.exports = verifyToken;
