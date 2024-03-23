d4b4e214dc476858c2fd09695b59ec1aad188897
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-23 14:29:14 +0100

    Update server.js

--- ecowise-travel/backend/server.js
+++ ecowise-travel/backend/server.js
@@ -1,7 +1,6 @@
-// server.js
-
 const express = require('express');
 const dotenv = require('dotenv');
+const { Pool } = require('pg');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
 // Import other routes as needed
 
@@ -14,6 +13,15 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
+
+// PostgreSQL database connection configuration
+const pool = new Pool({
+  user: process.env.ecowise_admin,
+  host: process.env.localhost,
+  database: process.env.ecowise_travel,
+  password: process.env.Ec0w!seP@ss,
+  port: process.env.3000,
+});
 
 // Mount routes
 app.use('/api/users', userRoutes);
@@ -28,4 +36,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
