0c45d1b0ada4b5c3af03d9688e2d56ec99534d4d
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-27 08:28:23 +0100

    Update App.js

--- ecowise-travel/src/App.js
+++ ecowise-travel/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+//import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
