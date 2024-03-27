43ff3df5076aa9e0fc93d18b0e0a8f12462450a8
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-27 08:47:51 +0100

    Update Navbar.js

--- ecowise-travel/src/components/Navbar.js
+++ ecowise-travel/src/components/Navbar.js
@@ -1,19 +1,24 @@
+// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/search" className="nav-link">Adventure Search</Link>
-      <Link to="/reviews" className="nav-link">Reviews</Link>
-      {/* Add links for other pages */}
-      <Link to="/travel-tips" className="nav-link">Travel Tips</Link>
-      <Link to="/adventures" className="nav-link">Adventures</Link>
-      <Link to="/community-forum" className="nav-link">Community Forum</Link>
-      <Link to="/impact-calculator" className="nav-link">Impact Calculator</Link>
+      <div className="logo">
+        <Link to="/">EcoWise Travel</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/adventures" className="nav-link">Adventures</Link>
+        <Link to="/impact-calculator" className="nav-link">Impact Calculator</Link>
+        <Link to="/travel-tips" className="nav-link">Travel Tips</Link>
+        <Link to="/community-forum" className="nav-link">Community Forum</Link>
+        <Link to="/reviews" className="nav-link">Review</Link>
+        <Link to="/Signin" className="nav-link">Sign in</Link>
+        <Link to="/Signup" className="nav-link">Sign up</Link>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
