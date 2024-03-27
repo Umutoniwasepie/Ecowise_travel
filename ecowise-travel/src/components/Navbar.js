b473357d33d46749b235ba5ff17b7d0c13cfc90f
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-27 15:29:02 +0100

    Updates

--- ecowise-travel/src/components/Navbar.js
+++ ecowise-travel/src/components/Navbar.js
@@ -1,24 +1,27 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">EcoWise Travel</Link>
+        <Link to="/">
+          <img src="./ecowise-logo.png" alt="EcoWise Travel Logo" /> {/* Assuming logo.png is in the same directory */}
+          <span className="ecowise-title">EcoWise Travel</span> {/* Green title without underline */}
+        </Link>
       </div>
       <div className="nav-links">
         <Link to="/adventures" className="nav-link">Adventures</Link>
         <Link to="/impact-calculator" className="nav-link">Impact Calculator</Link>
         <Link to="/travel-tips" className="nav-link">Travel Tips</Link>
         <Link to="/community-forum" className="nav-link">Community Forum</Link>
-        <Link to="/reviews" className="nav-link">Review</Link>
-        <Link to="/Signin" className="nav-link">Sign in</Link>
-        <Link to="/Signup" className="nav-link">Sign up</Link>
+        <Link to="/reviews" className="nav-link">Reviews</Link>
+        <Link to="/signin" className="nav-link">Sign In</Link>
+        <Link to="/signup" className="nav-link">Sign Up</Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
