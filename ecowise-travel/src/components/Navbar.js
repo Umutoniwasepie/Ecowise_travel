3228207858a46ed4367dd1778a9dce767d1d6f3e
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-27 15:41:19 +0100

    Update Navbar.js

--- ecowise-travel/src/components/Navbar.js
+++ ecowise-travel/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './ecowise-logo.png'; // Import logo image
+import logo from './logo192.png'; // Import logo image
 
 const Navbar = () => {
   return (
