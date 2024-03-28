754ffc84265d4130b6435e0ce5b45c10ac6ae081
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-28 01:01:10 +0100

    Update

--- ecowise-travel/src/pages/AdventureSearchPage.js
+++ ecowise-travel/src/pages/AdventureSearchPage.js
@@ -1,40 +1,39 @@
 import React from 'react';
-import AdventureCard from './AdventureCard'; // Import AdventureCard component
 import './Adventure.css'; // Import CSS file for AdventureSearchPage
 
 const adventuresData = [
   {
     id: 1,
     title: 'Explore the Volcanoes of Rwanda',
-    image: 'rwanda-volcanoes.jpg',
+    image: 'https://example.com/image1.jpg',
     description: 'Track gorillas in the mist and discover the stunning Rwandan landscape.',
     rating: 4.5
   },
   {
     id: 2,
     title: 'Turkey',
-    image: 'goreme-turkey.jpg',
+    image: 'https://example.com/image2.jpg',
     description: 'Immerse yourself in nature and explore ancient ruins on Turkish hiking trails.',
     rating: 4.0
   },
   {
     id: 3,
     title: 'Germany',
-    image: 'rheinhessen-germany.jpg',
+    image: 'https://example.com/image3.jpg',
     description: 'Experience the beauty of German forests on a scenic cycling adventure.',
     rating: 4.7
   },
   {
     id: 4,
     title: 'Costa Rica',
-    image: 'aguirre-costarica.jpg',
+    image: 'https://example.com/image4.jpg',
     description: 'Encounter diverse wildlife and support conservation efforts on a Costa Rican safari.',
     rating: 4.3
   },
   {
     id: 5,
     title: 'Norway',
-    image: 'green-norway.jpg',
+    image: 'https://example.com/image5.jpg',
     description: 'Explore Norway\'s stunning fjords and experience the beauty of nature by kayak.',
     rating: 4.8
   },
@@ -47,7 +46,7 @@ const AdventuresPage = () => {
       <div className="adventure-cards">
         {adventuresData.map((adventure) => (
           <div key={adventure.id} className="adventure-card">
-            <img src={adventure.image} alt={adventure.title} className="adventure-image" />
+            <div className="adventure-image" style={{ backgroundImage: `url(${adventure.image})` }}></div>
             <div className="adventure-details">
               <h3>{adventure.title}</h3>
               <p>{adventure.description}</p>
