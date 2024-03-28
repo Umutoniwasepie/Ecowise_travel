6009978ff4e85d5c8623c9db2d7c614c12c37258
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-28 01:15:14 +0100

    Update AdventureSearchPage.js

--- ecowise-travel/src/pages/AdventureSearchPage.js
+++ ecowise-travel/src/pages/AdventureSearchPage.js
@@ -5,35 +5,35 @@ const adventuresData = [
   {
     id: 1,
     title: 'Rwanda',
-    image: 'mariott-rwanda.jpg',
+    image: './mariott-rwanda.jpg',
     description: 'Track gorillas in the mist and discover the stunning Rwandan landscape.',
     rating: 4.7
   },
   {
     id: 2,
     title: 'Turkey',
-    image: 'goreme-turkey.jpg',
+    image: './goreme-turkey.jpg',
     description: 'Immerse yourself in nature and explore ancient ruins on Turkish hiking trails.',
     rating: 4.2
   },
   {
     id: 3,
     title: 'Germany',
-    image: 'rheinhessen-germany.jpg',
+    image: './rheinhessen-germany.jpg',
     description: 'Experience the beauty of German forests on a scenic cycling adventure.',
     rating: 4.6
   },
   {
     id: 4,
     title: 'Costa Rica',
-    image: 'aguirre-costarica.jpg',
+    image: './aguirre-costarica.jpg',
     description: 'Encounter diverse wildlife and support conservation efforts on a Costa Rican safari.',
     rating: 4.4
   },
   {
     id: 5,
     title: 'Norway',
-    image: 'green-norway.jpg',
+    image: './green-norway.jpg',
     description: 'Explore Norway\'s stunning fjords and experience the beauty of nature by kayak.',
     rating: 4.8
   },
