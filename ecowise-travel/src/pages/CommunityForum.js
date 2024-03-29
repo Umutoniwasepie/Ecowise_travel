5119dd4b16e7e6b487a1cc90ddffec7df1d7a03b
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-29 18:43:54 +0100

    Update

--- ecowise-travel/src/pages/CommunityForum.js
+++ ecowise-travel/src/pages/CommunityForum.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Fix the import statement
 
 const CommunityForum = () => {
   const [title, setTitle] = useState('');
