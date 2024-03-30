3313e75569eb99f60be1bbd45622cfb63f9ad45c
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-30 14:59:59 +0100

    Updates

--- ecowise-travel/eco-backend/routes/userRoutes.js
+++ ecowise-travel/eco-backend/routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models'); // Import the User model
+const { User } = require('../models/user'); // Import the User model
 
 // Route to create a new user
 router.post('/', async (req, res) => {
