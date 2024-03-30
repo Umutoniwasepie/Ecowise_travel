575baf97e3d37788c63b5a9485834d2afb575326
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-30 14:05:04 +0100

    models

--- ecowise-travel/eco-backend/models/user.js
+++ ecowise-travel/eco-backend/models/user.js
@@ -17,6 +17,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.Review, { foreignKey: 'userId' });
+  };
+
   return User;
 };
-
