72aab44e2548406ad22b8bb613f63afd02cd47b4
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-03-30 14:58:04 +0100

    Updates

---
+++ ecowise-travel/eco-backend/routes/reviewRoutes.js
@@ -0,0 +1,10 @@
+// routes/reviewRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const { Review } = require('../models'); // Import the Review model
+
+// Define routes for CRUD operations related to reviews
+
+// Export the router
+module.exports = router;
