05f1f81599fab24f87f92b0502cf9284f9f3abba
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-04-02 17:58:33 +0200

    Update

--- ecowise-travel/src/App.js
+++ ecowise-travel/src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Route path="/contact-form" element={<ContactForm />} />
           <Route path="*" element={null} />
         </Routes>
-        <ContactForm />
+      //  <ContactForm />
         <Footer />
       </div>
     </Router>
