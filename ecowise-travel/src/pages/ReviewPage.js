aea1f73125774a6945eff58e0224af55bcd9f8c3
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-04-02 21:15:44 +0200

    Updates

--- ecowise-travel/src/pages/ReviewPage.js
+++ ecowise-travel/src/pages/ReviewPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Reviewpage.css';
+import { Link } from 'react-router-dom';
 
 const ReviewPage = () => {
   const [review, setReview] = useState({
