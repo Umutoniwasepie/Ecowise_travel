47068515ad1c18ac3a5917d74df1851fb0007a2c
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-04-04 23:30:22 +0200

    Updates

--- ecowise-travel/src/pages/AdventureSearchPage.js
+++ ecowise-travel/src/pages/AdventureSearchPage.js
@@ -8,34 +8,34 @@ const adventuresData = [
     title: 'Rwanda',
     image: './gorilla-rwanda.jpg',
     description: 'Embark on a journey to Rwanda, the land of a thousand hills. Explore the lush rainforests of Volcanoes National Park and encounter the magnificent mountain gorillas in their natural habitat. Immerse yourself in the vibrant culture of Rwanda while supporting sustainable ecotourism initiatives that contribute to wildlife conservation and local community development.',
-    rating: 4.7
+    rating: 4.8
   },
   {
     id: 2,
     title: 'Turkey',
     image: './goreme-turkey.jpg',
-    description: 'Discover the breathtaking landscapes of Turkey, where ancient history meets stunning natural beauty. Traverse the scenic trails of Cappadocia and marvel at the otherworldly rock formations and fairy chimneys. Experience sustainable tourism practices in action as you stay in eco-friendly accommodations and engage in responsible travel activities that promote environmental conservation and cultural preservation.',
+    description: 'Take a hot air balloon ride over stunning rock formations in Cappadocia, a region rich in history. Stay in eco-friendly accommodations and participate in responsible activities that promote environmental and cultural preservation.',
     rating: 4.2
   },
   {
     id: 3,
     title: 'Germany',
     image: './rheinhessen-germany.jpg',
-    description: 'Embark on a sustainable adventure in Germany, where picturesque landscapes and eco-friendly practices abound. Cycle through the enchanting forests of Rheinhessen and explore charming villages nestled amidst vineyards and organic farms. Experience the essence of sustainable living as you indulge in locally sourced cuisine, stay in eco-lodges, and support initiatives focused on renewable energy and biodiversity conservation.',
+    description: 'Embark on an eco-friendly cycling adventure through charming villages nestled amidst vineyards. Enjoy locally sourced food, stay in eco-lodges, and support sustainable initiatives in a picturesque German wine region.',
     rating: 4.6
   },
   {
     id: 4,
     title: 'Costa Rica',
     image: './aguirre-costarica.jpg',
-    description: 'Experience the wonders of Costa Rica, a paradise for eco-conscious travelers seeking immersive nature experiences. Journey through lush rainforests teeming with biodiversity and encounter fascinating wildlife species, from sloths to toucans. Engage in responsible ecotourism activities such as canopy zip-lining, wildlife watching, and sustainable coffee tours, all while contributing to conservation efforts and community empowerment initiatives.',
+    description: 'Escape to a secluded island paradise, surrounded by crystal-clear waters teeming with marine life. Engage in responsible ecotourism activities like snorkeling and kayaking, contributing to conservation efforts while enjoying unforgettable experiences.',
     rating: 4.4
   },
   {
     id: 5,
     title: 'Norway',
     image: './green-norway.jpg',
-    description: 'Explore the pristine wilderness of Norway, where rugged mountains, crystal-clear fjords, and awe-inspiring glaciers await. Embark on eco-friendly adventures such as hiking, kayaking, and wildlife safaris, and immerse yourself in the tranquility of nature. Experience sustainable travel practices firsthand by staying in eco-lodges, dining on locally sourced cuisine, and participating in eco-conscious excursions that prioritize environmental stewardship and conservation.',
+    description: 'Explore awe-inspiring wilderness with majestic mountains, crystal-clear fjords, and cascading waterfalls. Embark on eco-friendly adventures like hiking and kayaking, and experience sustainable travel practices with eco-lodges and local cuisine.',
     rating: 4.8
   },
 ];
