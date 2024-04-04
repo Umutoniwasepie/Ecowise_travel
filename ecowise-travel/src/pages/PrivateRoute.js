38ebf0f00e1fe0664999cd153bdff6b698af9fa0
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-04-04 19:14:16 +0200

    Create PrivateRoute.js

---
+++ ecowise-travel/src/pages/PrivateRoute.js
@@ -0,0 +1,19 @@
+// PrivateRoute.js
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { token } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        token ? <Component {...props} /> : <Redirect to="/signin" />
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
