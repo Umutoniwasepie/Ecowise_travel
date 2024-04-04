78f4564334f405fa4ac0f4d9cdda8752d719b39f
Pierrette Umutoniwase <p.umutoniwa@alustudent.com>
2024-04-04 19:16:55 +0200

    new

---
+++ ecowise-travel/src/context/AuthContext.js
@@ -0,0 +1,29 @@
+// AuthContext.js
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token') || null);
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
+
+  const login = (token) => {
+    setToken(token);
+  };
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
+
