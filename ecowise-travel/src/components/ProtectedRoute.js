1ded23b7fa482bed817815f6fd651a850d47aafc
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2024-04-04 21:38:36 +0200

    Updates

--- ecowise-travel/src/components/ProtectedRoute.js
+++ ecowise-travel/src/components/ProtectedRoute.js
@@ -6,7 +6,7 @@ const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />; // Redirect to login on unauthorized access
+    return <Navigate to="/signin" replace />; // Redirect to login on unauthorized access
   }
 
   return <Outlet />; // Render the protected component if authenticated
